9a393e8e3cc70b961409eb082cde89f5c4b4a84d
Abel <abelrevelation@gmail.com>
2021-08-13 21:22:20 +0200

    Initial Commit

---
+++ App2/DetailPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App2.ViewModels
+{
+    public class DetailPageViewModel : INotifyPropertyChanged
+    {
+     
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DetailPageViewModel(String note)
+        { }
+
+        string noteText;
+        public string NoteText;
+        public Command DismissPageCommand { get; }
+    }
+}
---
+++ App2/MainPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App2.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            EraseCommand = new Command(() =>
+                {
+                    TheNote = string.Empty;
+                });
+
+            SavCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+                TheNote = string.Empty;
+            });
+        }
+        ObservableCollection<string> allNotes;
+        public ObservableCollection<string> AllNotes{
+            get
+            {
+                if (allNotes == null)
+                    allNotes = new ObservableCollection<string>();
+                return allNotes;
+            }
+            set
+            {
+                allNotes = value;
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        string theNote;
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
+        public Command SavCommand { get; }
+       public Command EraseCommand { get; }
+
+
+    }
+}
